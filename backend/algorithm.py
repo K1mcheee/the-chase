07ec36c8ea422487f5147e45d7a68a84811c27cc
K1mcheee <kimberlychiengyj@gmail.com>
2026-04-02 18:13:05 +0800

    add randomness to minimal cover

--- backend/algorithm.py
+++ backend/algorithm.py
@@ -1,7 +1,8 @@
+import random
 """"
 Attributes as String
 Set of Attributes as Set
-Functional Dependency as Pair/ List
+Functional Dependency as Pair/ Tuple
 
 R {"A", "B", "C", "D"}
 FDs [({"A"} , {"B"}), ({"C"} , {"A"})]
@@ -26,10 +27,6 @@ fd6 = [({"A", "B"}, {"C"}), ({"B", "C"}, {"C", "D"}), ({"C", "D"}, {"C", "D"}), 
 """
 Attribute closure algorithm
 """
-"""
-This is a good fixed-point algorithm (i.e., add until can no longer add).
-Some potential improvement is noted below.
-"""
 def algo1(R, FDs):
     # Attribute implies itself
     res = set(R)
@@ -40,16 +37,6 @@ def algo1(R, FDs):
     while update:
         update = False
         for lhs, rhs in FDs:
-            # --- ADI ---
-            # here, (lhs, rhs) in unused can only be true exactly once
-            # so there is no need to use loop
-            # instead, can simply check if (lhs, rhs) not in unused then continue
-            # -----------
-            """
-            OLD CODE:
-            while (lhs, rhs) in unused and lhs <= res:
-                unused.remove((lhs, rhs))
-            """
             if (lhs, rhs) not in unused:
                 continue
             if lhs <= res:  # (lhs, rhs) is in unused
@@ -60,50 +47,45 @@ def algo1(R, FDs):
     return res
 
 """
-minimal & canonical cover algorithm
+minimal cover algorithm
 """
 def algo2(FD):
-    tmp = FD.copy()
-
+    tmp = []
     # step 1: simplify RHS
     # this basically splits A -> BC into A -> B and A -> C
-    for lhs, rhs in tmp:
+    for lhs, rhs in FD:
         # split RHS if > 1 attribute
         if len(rhs) > 1:
-            curr1 = tmp.copy()
-            # remove FD first
-            curr1.remove((lhs, rhs))
-            # split RHS and add back FD
             for x in rhs:
-                curr1.append((lhs, set(x)))
-            tmp = curr1.copy()
+                tmp.append((lhs, set(x)))
+        else:
+            tmp.append((lhs, rhs))
 
     # step 2 simplify LHS
     # if we have AB -> C, remove A and see if we can still get C from B alone
     # if so, update FD to just B -> C
-    for lhs, rhs in tmp:
+    for i, (lhs, rhs) in enumerate(tmp):
         curr2 = tmp.copy()
+        tset = set(lhs)
         # if LHS > 1 attribute
         if len(lhs) > 1:
-            tset = lhs.copy()
-            for x in tset:
-                set1 = tset.copy()
+            llhs = list(lhs)
+            random.shuffle(llhs) # random removal of attribute
+            for x in llhs:
                 # remove curr attribute
-                set1.remove(x)
-                cls = algo1(set1, curr2)
+                cls = algo1(tset - {x}, curr2)
                 # if logically entail from rest of attribute, remove from FD
                 if rhs <= cls:
                     # update set to remove attribute
-                    tset = set1
-            curr2.remove((lhs, rhs))
-            # update FD w/o attribute
-            curr2.append((tset, rhs))
-        tmp = curr2.copy()
+                    tset = tset - {x}
+        tmp[i] = (tset, rhs)
 
     # step 3 simplify set
     # this removes duplicate FDs and those implied transitively
     # e.g. A -> B, B -> C, A -> C we remove A -> C
-    for lhs, rhs in tmp:  
+    randomtmp = tmp.copy()
+    random.shuffle(randomtmp)
+    for lhs, rhs in randomtmp:  
         curr = tmp.copy()
         # remove current FD from set
         curr.remove((lhs, rhs))
@@ -114,138 +96,6 @@ def algo2(FD):
             tmp = curr.copy()
     return tmp
 
-# convert a minimal cover to canonical
-def canonical(FD):
-    tmp = FD.copy()
-    res = []
-    used = []
-    for lhs, rhs in FD:
-        # current FD, remove from tmp
-        tmp.remove((lhs, rhs))
-        # for remaining FDs, find matching LHS
-        for tlhs, trhs in tmp:
-            if lhs == tlhs:
-                # update RHS, mark FD as used
-                used.append((tlhs, trhs))
-                rhs.update(trhs)
-        # add updated FDs/ original FDs that do not match        
-        if ((lhs, rhs) not in used):
-            res.append((lhs, rhs))
-        
-    return res
-
-"""
-finds candidate keys
-"""
-def candidate(attrs, FD):
-    # 1. Compute the minimal cover of the FD
-    mc = algo2(FD)
-    res = []
-    # 2. Find if there are any attribute not in RHS
-    tmpAttr = attrs.copy()
-    for lhs, rhs in mc:
-        if not tmpAttr:
-            # Short-circuit if set is empty
-            break
-        if rhs <= tmpAttr:
-            # attr appears in RHS, can remove
-            tmpAttr-=rhs
-    # Just to join LHS to make life easier
-    cc = canonical(mc)
-
-    # 3. compute closure of LHS
-    for lhs, rhs in cc:
-        # Any attribute not in RHS must appear in candidate key
-        nlhs = lhs.copy()
-        nlhs.update(tmpAttr)
-        closure = algo1(nlhs, FD)
-        # If S -> R then is candidate key
-        if closure <= attrs and nlhs not in res:
-            res.append(nlhs)
-
-    return res
-"""
-BCNF
-"""
-# helper for BCNF
-def powerset(s):
-    s = list(s)
-    result = [[]]
-    for elem in s:
-        result += [x + [elem] for x in result]
-    return [set(x) for x in result]
-
-# finds projected FD from set of attr - some attr
-# e.g. attrs = {A,B,C,D,E}, rm = C
-def projection(attrs, FD, rm):
-    res = []
-    tmp = []
-    # powerset to find all combination of attributes
-    pset = [p for p in powerset(attrs-rm) if p]
-
-    for s in pset:
-        # Compute closure of every combination of LHS
-        cls = algo1(s, FD)
-        tmp.append((s, cls))
-
-    for lhs, rhs in tmp:
-        # remove those containing the attr we dont want
-        if not rm.isdisjoint(rhs):
-            # Remove attr from RHS
-            rhs-=rm
-        # Subtract LHS from RHS
-        rhs-=lhs
-        if rhs <= lhs:
-        # Ignore trivial FDs
-            continue    
-        res.append((lhs, rhs))
-
-    return res
-
-def BCNF(attrs, FD):
-    bcnf = True
-    fragments = []
-
-    # Check for violation
-    for lhs, rhs in FD:
-        # Ignore trivial
-        if rhs <= lhs:
-            continue
-        
-        l_cls = algo1(lhs, FD)
-        # LHS not superkey
-        if not l_cls >= attrs:
-            bcnf = False
-            # Compute closure of LHS of violating FD
-            cls = algo1(lhs, FD) 
-            # Compute FDs to remove from R1
-            # {R} - closure of R1
-            rm1 = attrs.copy()-cls 
-            # Compute FDs to remove from R2
-            # Closure of R1 without R1
-            r2 = cls.copy()-lhs 
-            # Attributes going to R2
-            # {R} - closure of R1 + R1
-            r3 = rm1.copy()
-            r3.update(lhs)
-
-            # Find projection of each fragment
-            fd1 = projection(attrs, FD, rm1)
-            fd2 = projection(attrs, FD, r2)
-
-            # Recusrively check and split
-            _, attrs1 = BCNF(cls, fd1)
-            _, attrs2 = BCNF(r3, fd2)
-
-            # Add fragments to list
-            for x in attrs1:
-                fragments.append(x)
-            for x in attrs2:
-                fragments.append(x)
-            return (bcnf, fragments)
-    # Is in BCNF, add to list    
-    fragments.append(attrs)
-    return (bcnf, fragments)
 
 def make_table(attrs):
     row1 = {attr : f"a{j+1}" for j,attr in enumerate(attrs)} #create mapping A : a1 B: a2 etc.
@@ -293,8 +143,6 @@ fd = [({"A", "C"}, {"B", "D"}), ({"A", "D"}, {"C"}), ({"C"}, {"D"}), ({"C", "D"}
 # print("Minimal cover")
 # print(algo2(fd))
 
-# BCNF
-# print("\nBCNF")
 def parse_and_validate_fds(input):
     # Limit input size
     if len(input) > 1000:  # Arbitrary limit to prevent very large inputs
@@ -329,7 +177,6 @@ def parse_and_validate_fds(input):
     return attrSet, fds
 attrs, fds = parse_and_validate_fds("Abcde, ac->bd, ad->c, c->d, cd->e, e->d")
 
-# print(BCNF(attrs, fds))
 
 # Chase
 # print("\nChase")
